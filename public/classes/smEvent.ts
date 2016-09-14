790050184182df7d3eaea4de85b20d0f0d78f722
AndyRightNow <naruto950726@hotmail.com>
2016-09-15 00:42:30 +0800

    Basically finish EventComponent

--- public/classes/smEvent.ts
+++ public/classes/smEvent.ts
@@ -1,9 +1,22 @@
 export class smEvent {
-  title: string;
-  description: string;
-  startTime: number;
-  endTime: number;
-  isSelected: boolean;
-  type: string;
-  priority: number;
+  title: string = "";
+  description: string = "";
+  startTime: Object;
+  endTime: Object;
+  isSelected: boolean = false;
+  type: string = "";
+  priority: number = 0;
+
+  constructor() {
+    this.startTime = {
+      hour: 0,
+      minute: 0,
+      second: 0
+    };
+    this.endTime = {
+      hour: 0,
+      minute: 0,
+      second: 0
+    }
+  }
 }
--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -13,27 +13,30 @@ import { smEvent } from './../../../public/classes/smEvent';
 })
 export class EventComponent implements OnInit, DoCheck {
 	@Input() event: smEvent;
-	@Input() startHour: number = 0;
-	@Input() startMin: number = 0;
-	@Input() startSec: number = 0;
-	@Input() endHour: number = 0;
-	@Input() endMin: number = 0;
-	@Input() endSec: number = 0;
 	private isTitleEditing: boolean = true;
 	private isDescEditing: boolean = true;
 	private isStartTimeEditing: boolean = true;
 	private isEndTimeEditing: boolean = true;
+	private priorityArray: boolean[] = new Array(5).fill(false);
+
+	private formatTime(time: number): string {
+		return time.toString().split('').map((val, index, arr) => {
+			return index == 0 && arr.length == 1 ? "0" + val : val;
+		}).join('');
+	}
+
+	changePriority(i: number) {
+		this.priorityArray = this.priorityArray.map((val, index) => index > i ? false : true);
+		this.event.priority = i + 1;
+	}
 
 	constructor() { 
 		this.event = new smEvent();
-
 	}
 
 	ngOnInit() {
 	}
 
 	ngDoCheck() {
-		this.event.startTime = this.startHour * 3600 + this.startMin * 60 + this.startSec * 1; 
-		this.event.endTime = this.endHour * 3600 + this.endMin * 60 + this.endSec * 1;
 	}
 }
