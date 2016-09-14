9171bd0c2ac081de46949626ff7b47704f0022b6
AndyRightNow <naruto950726@hotmail.com>
2016-09-14 15:52:19 +0800

    Modify event layout

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,7 +1,8 @@
 import {
   Component,
 	Input,
-	OnInit
+	OnInit,
+	DoCheck
 } from '@angular/core';
 import { smEvent } from './../../../public/classes/smEvent';
 
@@ -10,8 +11,14 @@ import { smEvent } from './../../../public/classes/smEvent';
   templateUrl: './event.component.html',
 	styleUrls: ['./event.component.css']
 })
-export class EventComponent implements OnInit {
+export class EventComponent implements OnInit, DoCheck {
 	@Input() event: smEvent;
+	@Input() startHour: number = 0;
+	@Input() startMin: number = 0;
+	@Input() startSec: number = 0;
+	@Input() endHour: number = 0;
+	@Input() endMin: number = 0;
+	@Input() endSec: number = 0;
 	private isTitleEditing: boolean = true;
 	private isDescEditing: boolean = true;
 	private isStartTimeEditing: boolean = true;
@@ -23,5 +30,10 @@ export class EventComponent implements OnInit {
 	}
 
 	ngOnInit() {
+	}
+
+	ngDoCheck() {
+		this.event.startTime = this.startHour * 3600 + this.startMin * 60 + this.startSec * 1; 
+		this.event.endTime = this.endHour * 3600 + this.endMin * 60 + this.endSec * 1;
 	}
 }
