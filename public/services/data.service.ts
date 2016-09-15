1e6ddcaaf45c713a47380bc6795d309d597d0a6b
AndyRightNow <naruto950726@hotmail.com>
2016-09-15 20:28:19 +0800

    Add comments

--- public/services/data.service.ts
+++ public/services/data.service.ts
@@ -1,37 +1,70 @@
+/*
+ * Provide in-memory data control
+ */
+
 import { smEvent } from './../classes/smEvent';
 import { Utils } from './../classes/utils';
 
 export class DataService {
-  private data: smEvent[];
+  // The data in memory
+  private data: smEvent[] = [];
 
-  constructor() {
-    this.data = [];
-  }
+  constructor() { }
 
+  /*
+   * Update all ids of elements from a position on
+   *
+   * @param {number} start: Starting index
+   */
   private assignIds(start: number) {
     for (let i = start; i < this.data.length; i++)
         this.data[i].id = i;
   }
 
+  /*
+   * Return a reference of the data array
+   *
+   */
   public getData(): smEvent[] {
     return this.data;
   }
 
+  /*
+   * Add an event to the back of the data array
+   *
+   * @param {smEvent} event: The event to add to the data array
+   */
   public add(event: smEvent) {
     this.data.push(event);
     event.id = this.data.length - 1;
   }
 
+  /*
+   * Delete an event(events) at(from) a certain position
+   *
+   * @param {number} pos: The start position of the event(s) to delete
+   * @param {number} deleteCount: The number of elements to delete
+   */
   public delete(pos: number, deleteCount: number = 1) {
     this.data.splice(pos, deleteCount);
     this.assignIds(pos);
   }
 
+  /*
+   * Insert an event at a certain position
+   *
+   * @param {number} pos: The position to insert at
+   * @param {smEvent} event: The event to insert
+   */  
   public insert(pos: number, event: smEvent) {
     this.data.splice(pos, 0, event);
     this.assignIds(pos);
   }
 
+  /*
+   * Sort all events in the order of priorities first and start time second
+   *
+   */  
   public sort() {
     this.data.sort((a, b) => {
       return a.priority == b.priority ?
--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -28,32 +28,52 @@ export class EventComponent implements OnInit, DoCheck {
 	// Visual presentation of priority
 	private priorityArray: boolean[] = new Array(5).fill(false);
 
-		
+	/*
+	 * Add padding zeros to time units(hour, minute or second)
+	 *
+	 * @return {string} formatted time
+	 * @param {number | string} time: Input time
+	 */
 	private formatTime(time: number | string): string {
 		return time == "" ? "00" : time.toString().split('').map((val, index, arr) => {
 			return index == 0 && arr.length == 1 ? "0" + val : val;
 		}).join('');
 	}
 
+	/*
+	 * Change priority based on highlighted circles that users select
+	 *
+	 */	
 	private changePriority(i: number) {
 		this.priorityArray = this.priorityArray.map((val, index) => index > i ? false : true);
 		this.event.priority = i + 1;
 	}
 
+	/*
+	 * Delete this event
+	 *
+	 */	
 	private deleteEvent() {
 		if (!event) return;
 		this.dataService.delete(this.event.id);
 	}
 
+	/*
+	 * Constructor
+	 *
+	 * @param {DataService} dataService: The DataService single instance
+	 */		
 	constructor(private dataService: DataService) { }
 
 	ngOnInit() { }
 
 	ngDoCheck() {
+		//-------------------------------------------------
+		// Normalize and clamp users input time
+		//-------------------------------------------------
 		this.event.startTime.hour = isNaN(this.event.startTime.hour) ? 0 : Utils.clamp(this.event.startTime.hour, 0, 23);
 		this.event.startTime.minute = isNaN(this.event.startTime.minute) ? 0 : Utils.clamp(this.event.startTime.minute, 0, 59);
 		this.event.startTime.second = isNaN(this.event.startTime.second) ? 0 : Utils.clamp(this.event.startTime.second, 0, 59);
-
 		this.event.endTime.hour = isNaN(this.event.endTime.hour) ? 0 : Utils.clamp(this.event.endTime.hour, 0, 23);
 		this.event.endTime.minute = isNaN(this.event.endTime.minute) ? 0 : Utils.clamp(this.event.endTime.minute, 0, 59);
 		this.event.endTime.second = isNaN(this.event.endTime.second) ? 0 : Utils.clamp(this.event.endTime.second, 0, 59);
